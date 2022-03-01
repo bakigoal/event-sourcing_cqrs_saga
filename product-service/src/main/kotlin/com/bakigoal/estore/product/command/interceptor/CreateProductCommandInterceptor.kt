489ab6e0fcace36baa6dc92776e6c6944b5d0f99
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-01 23:14:45 +0500

    register interceptor

--- product-service/src/main/kotlin/com/bakigoal/estore/product/command/interceptor/CreateProductCommandInterceptor.kt
+++ product-service/src/main/kotlin/com/bakigoal/estore/product/command/interceptor/CreateProductCommandInterceptor.kt
@@ -1,23 +1,34 @@
 package com.bakigoal.estore.product.command.interceptor
 
 import com.bakigoal.estore.product.command.CreateProductCommand
+import org.axonframework.commandhandling.CommandBus
 import org.axonframework.commandhandling.CommandMessage
 import org.axonframework.messaging.MessageDispatchInterceptor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.lang.IllegalArgumentException
 import java.util.function.BiFunction
+import javax.annotation.PostConstruct
 
 @Component
-class CreateProductCommandInterceptor : MessageDispatchInterceptor<CommandMessage<Any>> {
+class CreateProductCommandInterceptor : MessageDispatchInterceptor<CommandMessage<*>> {
 
     companion object{
         val logger: Logger = LoggerFactory.getLogger(CreateProductCommandInterceptor::class.java)
     }
 
-    override fun handle(messages: MutableList<out CommandMessage<Any>>?) =
-        BiFunction<Int, CommandMessage<Any>, CommandMessage<Any>> { index, command ->
+    @Autowired
+    lateinit var commandBus: CommandBus
+
+    @PostConstruct
+    fun init() {
+        commandBus.registerDispatchInterceptor(this)
+    }
+
+    override fun handle(messages: MutableList<out CommandMessage<*>>?) =
+        BiFunction<Int, CommandMessage<*>, CommandMessage<*>> { index, command ->
             val payload = command.payload
             logger.info("Intercepted $payload")
             if (payload is CreateProductCommand){
