798ab2b04ffab3f33200bbdc4727dd57599ebbe5
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-01 18:46:27 +0500

    split rest to query and command requests

--- product-service/src/main/kotlin/com/bakigoal/estore/product/command/rest/ProductCommandController.kt
+++ product-service/src/main/kotlin/com/bakigoal/estore/product/command/rest/ProductCommandController.kt
@@ -1,31 +1,21 @@
-package com.bakigoal.estore.product.rest
+package com.bakigoal.estore.product.command.rest
 
 import com.bakigoal.estore.product.command.CreateProductCommand
 import com.bakigoal.estore.product.dto.Product
-import com.bakigoal.estore.product.repo.ProductRepo
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/product")
-class ProductController(
-    val env: Environment,
-    val commandGateway: CommandGateway,
-    val productRepo: ProductRepo
+class ProductCommandController(
+    val commandGateway: CommandGateway
 ) {
 
     companion object {
-        val logger: Logger = LoggerFactory.getLogger(ProductController::class.java)
-    }
-
-    @GetMapping
-    fun get(): List<Product> {
-        logger.info("get product")
-        return productRepo.findAll().map { Product(title = it.title, price = it.price, quantity = it.quantity) }
+        val logger: Logger = LoggerFactory.getLogger(ProductCommandController::class.java)
     }
 
     @PostMapping
---
+++ product-service/src/main/kotlin/com/bakigoal/estore/product/query/rest/ProductQueryController.kt
@@ -0,0 +1,28 @@
+package com.bakigoal.estore.product.query.rest
+
+import com.bakigoal.estore.product.dto.Product
+import com.bakigoal.estore.product.repo.ProductRepo
+import org.axonframework.commandhandling.gateway.CommandGateway
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/product")
+class ProductQueryController(
+    val commandGateway: CommandGateway,
+    val productRepo: ProductRepo
+) {
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(ProductQueryController::class.java)
+    }
+
+    @GetMapping
+    fun get(): List<Product> {
+        logger.info("get product")
+        return productRepo.findAll().map { Product(title = it.title, price = it.price, quantity = it.quantity) }
+    }
+}
