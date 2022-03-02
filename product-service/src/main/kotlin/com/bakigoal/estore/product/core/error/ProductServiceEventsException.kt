91e0e1bddd8e86b26ac0d5cd4aee39bec573131a
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-02 12:47:19 +0500

    add exception handler for eventHandler

---
+++ product-service/src/main/kotlin/com/bakigoal/estore/product/core/error/ProductServiceEventsException.kt
@@ -0,0 +1,5 @@
+package com.bakigoal.estore.product.core.error
+
+import java.lang.RuntimeException
+
+class ProductServiceEventsException(message: String?): RuntimeException(message)
--- product-service/src/main/kotlin/com/bakigoal/estore/product/query/ProductEventsHandler.kt
+++ product-service/src/main/kotlin/com/bakigoal/estore/product/query/ProductEventsHandler.kt
@@ -1,10 +1,12 @@
 package com.bakigoal.estore.product.query
 
 import com.bakigoal.estore.product.core.entity.ProductEntity
+import com.bakigoal.estore.product.core.error.ProductServiceEventsException
 import com.bakigoal.estore.product.core.events.ProductCreatedEvent
 import com.bakigoal.estore.product.core.repo.ProductRepo
 import org.axonframework.config.ProcessingGroup
 import org.axonframework.eventhandling.EventHandler
+import org.axonframework.messaging.interceptors.ExceptionHandler
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -29,5 +31,11 @@ class ProductEventsHandler(
         )
         logger.info("saving product $entity")
         productRepo.save(entity)
+    }
+
+    @ExceptionHandler(resultType = Exception::class)
+    fun handleException(e: Exception) {
+        logger.error("Exception on EventHandler method: ${e.message}")
+        throw ProductServiceEventsException(e.message)
     }
 }
